af4afe091eda42770978fece0e694eece31c8d78
Shikhar15606 <shikharshukla15606@gmail.com>
2021-03-19 21:19:03 +0530

    Passing Data between Screens as Params

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import CATEGORIES from '../data/dummyData';
+
 const CategoryMealsScreen = props => {
+  const categoryId = props.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find(
+    category => category.id === categoryId
+  );
   return (
     <View style={styles.screen}>
       <Text>CategoryMealsScreen</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title='Go to Meal Details'
         onPress={() => {
