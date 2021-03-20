388a9a2fed64f8581b6a1254ac0e07e0b2cef37e
Shikhar15606 <shikharshukla15606@gmail.com>
2021-03-21 01:19:45 +0530

    Bottom Tabbed Navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,11 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Platform } from 'react-native';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavouritesScreen from '../screens/FavouritesScreen';
 import Colors from '../constants/colors';
 
 import { enableScreens } from 'react-native-screens';
@@ -30,4 +32,9 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favourites: FavouritesScreen,
+});
+
+export default createAppContainer(MealsFavTabNavigator);
