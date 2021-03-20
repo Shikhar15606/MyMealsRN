70ecf2096c57f97d9f66e350688c565407d4c077
Shikhar15606 <shikharshukla15606@gmail.com>
2021-03-21 01:01:12 +0530

    react-navigation-header-buttons@3.0.1

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { CATEGORIES, MEALS } from '../data/dummyData';
+import { View, Text, StyleSheet } from 'react-native';
+import { MEALS } from '../data/dummyData';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
@@ -16,8 +18,20 @@ const MealDetailScreen = props => {
 MealDetailScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam('mealId');
   const meal = MEALS.find(meal => meal.id === mealId);
+
   return {
     title: meal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title='Favorite'
+          iconName='ios-star'
+          onPress={() => {
+            console.log('Mark as favorite!');
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 const styles = StyleSheet.create({
