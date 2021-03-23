64eabea7cefd155354273c161ddf7e3b8dfc9822
Shikhar15606 <shikharshukla15606@gmail.com>
2021-03-24 01:27:04 +0530

    No fav screen content

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -3,9 +3,18 @@ import MealList from '../components/MealList';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import { useSelector } from 'react-redux';
+import DefaultText from '../components/DefaultText';
+import { View, StyleSheet } from 'react-native';
 
 const FavouritesScreen = props => {
   const displayedMeals = useSelector(state => state.meals.favoriteMeals);
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.screen}>
+        <DefaultText>No favourite dishes found. Add Some ! </DefaultText>
+      </View>
+    );
+  }
   return <MealList data={displayedMeals} navigation={props.navigation} />;
 };
 
@@ -25,4 +34,12 @@ FavouritesScreen.navigationOptions = navData => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default FavouritesScreen;
